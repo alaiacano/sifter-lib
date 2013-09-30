35fdb6446b57cdc8a0cd85e4b96a812ea005f850
Adam Laiacano <adam@tumblr.com>
2013-09-29 23:46:14 -0400

    more shuffling files around

---
+++ src/test/scala/cc/sifter/AnnealingEpsilonGreedySpec.scala
@@ -0,0 +1,39 @@
+package cc.sifter
+
+import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers._
+
+import java.util.Random
+
+class AnnealingEpsilonGreedySpec extends FlatSpec {
+  val rand = new Random(1)
+  
+  "An AnnealingEpsilonGreedy algorithm" should "produce the right steady state output" in {
+      
+    val Npulls = 10000
+    val test = AnnealingEpsilonGreedy(Seq(Arm("one"), Arm("two"), Arm("three")))
+    
+    for (i <- 1 to Npulls) {
+      val arm = test.selectArm()
+      val prob = arm.getId match {
+        case "one"   => .2
+        case "two"   => .4
+        case "three" => .8   // this should be the highest
+      }
+
+      if (rand.nextDouble < prob) {
+          test.success(arm, 1.0)
+      }
+      else {
+          test.failure(arm, 1.0)
+      }
+    }
+
+    test.arms(2).getPullCount should be > (test.arms(0).getPullCount)
+    test.arms(2).getPullCount should be > (test.arms(1).getPullCount)
+
+    test.arms(2).getValue should be > (test.arms(0).getValue)
+    test.arms(2).getValue should be > (test.arms(1).getValue)
+    test.arms(1).getValue should be > (test.arms(0).getValue)
+  }
+}
